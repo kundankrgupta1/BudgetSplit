68c2f9aabba9308ebc4319a4d6d68d6fdb6564f5
Kundan Kumar Gupta <kundankrgupta@outlook.com>
2025-04-28 12:10:01 +0530

    backend url updates

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:3000/api', // Replace with your backend URL
+  baseURL: 'https://budgetsplit-1.onrender.com/api',
+//   baseURL: 'http://localhost:3000/api',
   withCredentials: true, 
 });
 
